4ba4392eee604555f02df8f80c07bb3e9bfbf8ae
ecapirchio <122386726+ecapirchio@users.noreply.github.com>
2025-03-05 15:52:13 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using BlockBusterWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BlockBusterWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Colors()
+        {
+            string[] colors = { "Red", "Blue", "Yellow" };
+            ViewBag.Colors = colors;
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            ViewBag.Cities = new List<string>
+            {
+                "Tokyo, Japan",
+                "Paris, France",
+                "New York City, USA",
+                "Rome, Italy",
+                "Sydney, Australia"
+            };
+
+            return View();
+        }
+
+        public IActionResult Hobbies()
+        {
+            ViewBag.Hobbies = new List<string>
+            {
+                "Hiking",
+                "Basketball",
+                "Playing Piano",
+                "Coding",
+                "Traveling"
+            };
+
+            return View();
+        }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
